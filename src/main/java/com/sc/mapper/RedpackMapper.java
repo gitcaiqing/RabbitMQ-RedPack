ed3ebfd95dbec37f60ba6b646783dad4e1479c03
gitcaiqing <380120634@qq.com>
2018-11-30 18:05:36 +0800

    Spring RabbitMQ 整合案例之抢红包（高并发）

--- src/main/java/com/sc/mapper/RedpackMapper.java
+++ src/main/java/com/sc/mapper/RedpackMapper.java
@@ -3,40 +3,17 @@ package com.sc.mapper;
 import com.sc.entity.Redpack;
 
 public interface RedpackMapper {
-    /**
-     *
-     * @mbggenerated 2018-11-26
-     */
+	
     int deleteByPrimaryKey(Integer id);
-
-    /**
-     *
-     * @mbggenerated 2018-11-26
-     */
+    
     int insert(Redpack record);
-
-    /**
-     *
-     * @mbggenerated 2018-11-26
-     */
+    
     int insertSelective(Redpack record);
-
-    /**
-     *
-     * @mbggenerated 2018-11-26
-     */
+    
     Redpack selectByPrimaryKey(Integer id);
-
-    /**
-     *
-     * @mbggenerated 2018-11-26
-     */
+    
     int updateByPrimaryKeySelective(Redpack record);
-
-    /**
-     *
-     * @mbggenerated 2018-11-26
-     */
+    
     int updateByPrimaryKey(Redpack record);
     
     /**
--- src/main/java/com/sc/mapper/UserRedpackMapper.java
+++ src/main/java/com/sc/mapper/UserRedpackMapper.java
@@ -3,39 +3,15 @@ package com.sc.mapper;
 import com.sc.entity.UserRedpack;
 
 public interface UserRedpackMapper {
-    /**
-     *
-     * @mbggenerated 2018-11-26
-     */
     int deleteByPrimaryKey(Integer id);
-
-    /**
-     *
-     * @mbggenerated 2018-11-26
-     */
+    
     int insert(UserRedpack record);
-
-    /**
-     *
-     * @mbggenerated 2018-11-26
-     */
+    
     int insertSelective(UserRedpack record);
-
-    /**
-     *
-     * @mbggenerated 2018-11-26
-     */
+    
     UserRedpack selectByPrimaryKey(Integer id);
-
-    /**
-     *
-     * @mbggenerated 2018-11-26
-     */
+    
     int updateByPrimaryKeySelective(UserRedpack record);
-
-    /**
-     *
-     * @mbggenerated 2018-11-26
-     */
+    
     int updateByPrimaryKey(UserRedpack record);
 }
